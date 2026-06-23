97211c89015bf4d67581236a17a4b63b6d2196ab
Alexandros Paliampelos <f5890620@gmail.com>
2026-06-23 21:27:54 +0300

    remove unused Url case from SettingType enum

--- app/Enums/SettingType.php
+++ app/Enums/SettingType.php
@@ -6,7 +6,6 @@ enum SettingType: string
 {
     case String = 'string';
     case Text = 'text';
-    case Url = 'url';
     case Color = 'color';
     case Switch = 'switch';
     case Number = 'number';
